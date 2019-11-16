e21fa7d2b19a3345e722219f8397ddd37d145180
ash1105 <sub.ageha24@gmail.com>
2019-11-17 01:47:36 +0900

    リターンコードが0ではないので明示的に正常終了させるように修正する。また、Panicをやめる

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/higuching/slack_railway_bot/app"
+	"os"
 )
 
 func main() {
 	_, err := app.Run()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
+	os.Exit(0)
 }
