15eb24d1bfec2f477b2e0cc198dd8813c18f7566
higuching <day.dreamin.faze@gmail.com>
2019-11-16 15:38:01 +0900

    aws lambdaを使用しないブランチ作成

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/higuching/slack_railway_bot/app"
 )
 
-func railways() (string, error) {
-	text, err := app.Run()
-	return text, err
-}
-
 func main() {
-	lambda.Start(railways)
+	_, err := app.Run()
+	if err != nil {
+		panic(err)
+	}
 }
